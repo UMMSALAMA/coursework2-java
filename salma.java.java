cb3b8ade065f0fde760d17adee12c672cf350cb4
ZalimAbdul <47660128+Ummsalama@users.noreply.github.com>
2019-02-23 11:02:06 +0300

    Update salma.java.java

--- salma.java.java
+++ salma.java.java
@@ -1,51 +1,43 @@
-import java.util.Scanner;
 
-public class Salma{
+import java.util.ArrayList;
+
+public class javaMain {
 
 	public static void main(String[] args) {
-		Scanner Scn=new Scanner(System.in);
+		// TODO Auto-generated method stub
+		ArrayList<String> names =  new ArrayList<String>();
+		ArrayList<String> cses =  new ArrayList<String>();
+		ArrayList<Double> marks =  new ArrayList<Double>();
+		int k= 1;
+		double SUM = 0;
+		double av = 0;
+		while(true) {
+			System.out.print("Enter student "+k+" name:");
+			String name = new java.util.Scanner(System.in).nextLine();
+			int c=0;
+			while(c<5) {
+				System.out.print("Enter course Unit "+(c+1)+":");
+				String cse = new java.util.Scanner(System.in).nextLine();
+				System.out.print("Enter marks :");
+				Double mark = new java.util.Scanner(System.in).nextDouble();
+				SUM = SUM + mark;
+				if(name.toLowerCase().equals("exit"))
+						break;
+				else
+					names.add(name);
+					marks.add(mark);
+					cses.add(cse);		
+			c++;
+			}
 
-		System.out.println("enter java2");
-		Double num1=Scn.nextDouble();
-		System.out.println("enter computer application");
-		Double num2=Scn.nextDouble();
-		System.out.println("enter operating system");
-		Double num3=Scn.nextDouble();
-		System.out.println("enter calculus");
-		Double num4=Scn.nextDouble();
-		System.out.println("enter web design2");
-		Double num5=Scn.nextDouble();
-		System.out.println("enter computer networks");
-		Double num6=Scn.nextDouble();
-		System.out.println("enter communication skills");
-		Double num7=Scn.nextDouble();
-		Double sum;
-		sum=num1+num2+num3+num4+num5+num6+num7;
-		System.out.println("sum ="+ sum);
-		Double avg;
-		avg=sum/7;
-		System.out.println("avg ="+ avg);
-		
-		if(avg>=90 && avg<100)
-		{
-			System.out.println("GPA="+5.0);
+			System.out.println("name : " + names.get(0));
+			System.out.println("cdfg : " + cses.get(0));
+
+			System.out.println("total = "+SUM);
+			av = SUM/5;
+			System.out.println(" The average is : "+av);
+
+			k++;
 		}
-		else if(avg>=80 && avg<90)
-		{
-			System.out.println("GPA="+4.0);
-		}
-		else if(avg>=70 && avg<80)
-		{
-			System.out.println("GPA="+3.0);
-		}
-		else if(avg>=60 && avg<70)
-		{
-			System.out.println("GPA="+2.0);
-		}
-		else {
-			System.out.println("fail");
-		}
-		
 	}
-
 }
